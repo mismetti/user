7cd0848610d4297afb8eebd53507225fc939eb2e
mismetti <milastrokes@gmail.com>
2024-12-02 17:24:12 -0300

    converter added

---
+++ src/main/java/com/mila/user/business/dto/UserDTO.java
@@ -0,0 +1,20 @@
+package com.mila.user.business.dto;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserDTO {
+
+    private String name;
+    private String email;
+    private String password;
+    private List<AddressDTO> addresses;
+    private List<PhoneDTO> phones;
+
+}
